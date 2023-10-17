9d22da905c98f7ef826a3fbd566f7c76f64ea4e0
Merhong <zx2938@gmail.com>
2023-10-17 09:34:20 +0900

    231017

---
+++ lib/_core/constants/move.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/ui/pages/auth/join_page/join_page.dart';
+import 'package:flutter_blog/ui/pages/auth/login_page/login_page.dart';
+import 'package:flutter_blog/ui/pages/post/list_page/post_list_page.dart';
+import 'package:flutter_blog/ui/pages/post/write_page/post_write_page.dart';
+
+// 실수하지 말라고 static으로 만들어둠.
+// Move안에 모아둔 이유? 관리하기 편해서
+class Move {
+  static String loginPage = "/login";
+  static String joinPage = "/join";
+  static String postListPage = "/post/list";
+  static String postWritePage = "/post/write";
+  static String userInfoPage = "/user/info";
+}
+
+// Router는 Map타입 리턴 받아야함
+// function대신 dynamic으로 적어도 된다.
+Map<String, Widget Function(BuildContext)> getRouters() {
+  return {
+    Move.loginPage: (context) => const LoginPage(),
+  Move.joinPage: (context) => const JoinPage(),
+  Move.postListPage: (context) => PostListPage(),
+  Move.postWritePage: (context) => const PostWritePage(),
+};
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/constants/move.dart';
+import 'package:flutter_blog/_core/constants/theme.dart';
+
+
+// TODO 1: Widget이 아닌 곳에서 현재 화면의 Context에 접근해주는 객체
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey, // context가 없는 곳에서 context를 사용할 수 있는 방법 (몰라도 됨)
+      debugShowCheckedModeBanner: false,
+      initialRoute: Move.loginPage,
+      routes: getRouters(),
+      theme: theme(),
+    );
+  }
+}
